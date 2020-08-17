ed8969288921559d6a54819a50c84a3afee2a06d
pfroelke <patryk.froelke@gmail.com>
2020-08-17 19:31:53 +0200

    style: switch from css to scss feat: add routing BREAKING CHANGE: change project name

--- website/frontend/src/app/app.module.ts
+++ website/frontend/src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule} from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './layout/app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HolyGrailLayoutComponent } from './layout/holy-grail-layout/holy-grail-layout.component';
+import { IndexComponent } from './pages/index/index.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule
-  ],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  declarations: [AppComponent, HolyGrailLayoutComponent, IndexComponent],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
