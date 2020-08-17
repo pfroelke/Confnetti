3910a48b9eebefb4dac1bdc6cde026de0f3d070f
pfroelke <50522783+pfroelke@users.noreply.github.com>
2020-08-17 19:31:53 +0200

    style: switch from css to scss feat: add routing BREAKING CHANGE: change project name

---
+++ website/frontend/src/app/layout/holy-grail-layout/holy-grail-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-holy-grail-layout',
+  templateUrl: './holy-grail-layout.component.html',
+  styleUrls: ['./holy-grail-layout.component.scss'],
+})
+export class HolyGrailLayoutComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
