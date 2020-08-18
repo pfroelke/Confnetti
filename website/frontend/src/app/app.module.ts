812a17f748e9675c62008f14939260db4deb1311
pfroelke <50522783+pfroelke@users.noreply.github.com>
2020-08-18 20:01:31 +0200

    feat: add holy-grail-layout

--- website/frontend/src/app/app.module.ts
+++ website/frontend/src/app/app.module.ts
@@ -6,10 +6,14 @@ import { AppComponent } from './layout/app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HolyGrailLayoutComponent } from './layout/holy-grail-layout/holy-grail-layout.component';
 import { IndexComponent } from './pages/index/index.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { TestPageComponent } from './pages/test-page/test-page.component';
 
 @NgModule({
   imports: [BrowserModule, HttpClientModule, AppRoutingModule],
-  declarations: [AppComponent, HolyGrailLayoutComponent, IndexComponent],
+  declarations: [AppComponent, HolyGrailLayoutComponent, IndexComponent, FooterComponent, HeaderComponent, SidenavComponent, TestPageComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
