aad76fa8a13a4d9380c646d506de2a1b966f20bc
pfroelke <patryk.froelke@gmail.com>
2020-08-24 16:16:13 +0200

    refractor: move tasks

--- website/backend/confnetti/api/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Task(models.Model):
-    name = models.CharField(max_length=32)
-    desc = models.CharField(max_length=256)
--- website/backend/confnetti/tasks/models.py
+++ website/backend/confnetti/tasks/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    name = models.CharField(max_length=32)
+    desc = models.CharField(max_length=256)
