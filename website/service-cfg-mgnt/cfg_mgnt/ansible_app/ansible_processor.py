96ed4c2b7f0f215a95baf45cf46a0a6d0b9750e9
zouker <kontakt.komorowski@gmail.com>
2020-11-08 12:46:39 +0100

    part2 ansible integration

--- website/service-cfg-mgnt/cfg_mgnt/ansible_app/ansible_processor.py
+++ website/service-cfg-mgnt/cfg_mgnt/ansible_app/ansible_processor.py
@@ -1,7 +1,27 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import TaskSerializer
+from .models import AnsibleTask
+
+import json
+import shutil
+import ansible_runner
 
 
-def dupa():
-    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+
+class AnsibleProcessor:
+
+    def start_remaining_tasks(self):
+        db_ansible_tasks = AnsibleTask.objects.all()
+        for db_ansible_task in db_ansible_tasks:
+            if db_ansible_task.status == "queued":
+                db_ansible_task.status = "processing"
+                db_ansible_task.save()
+                print("now task being processed")
+                self.run_ansible_task()
+
+    def run_ansible_task(self):
+        r = ansible_runner.run(private_data_dir='/code/ansible_app/ansible_store', playbook='playbook.yml')
+        print("{}: {}".format(r.status, r.rc))
+
+
