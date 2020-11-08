93c64411c5fc532278c94b15ee80ae5710c52a0b
szymonkomorowski <szymon.komorowski@gmail.com.com>
2020-11-08 12:46:39 +0100

    part2 ansible integration

--- website/service-cfg-mgnt/cfg_mgnt/ansible_app/views.py
+++ website/service-cfg-mgnt/cfg_mgnt/ansible_app/views.py
@@ -3,7 +3,7 @@ from .models import Playbook, AnsibleTask
 from django.forms import forms
 from rest_framework import generics, status
 from .serializers import TaskSerializer
-from.ansible_processor import dupa
+from.ansible_processor import AnsibleProcessor
 from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
 import time
@@ -24,13 +24,12 @@ class TaskView(generics.ListCreateAPIView):
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-        print(serializer.initial_data["task_id"])
-        time.sleep(2)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
 
-        dupa()
+        ap = AnsibleProcessor()
+        ap.start_remaining_tasks()
 
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
