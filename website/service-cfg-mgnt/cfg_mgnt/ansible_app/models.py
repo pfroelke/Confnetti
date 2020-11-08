8a6bd5bc7337ccac6dd8fda1b3c25eda83363dce
szymonkomorowski <szymon.komorowski@gmail.com.com>
2020-11-08 03:14:50 +0100

    first part ansible handling

--- website/service-cfg-mgnt/cfg_mgnt/ansible_app/models.py
+++ website/service-cfg-mgnt/cfg_mgnt/ansible_app/models.py
@@ -2,8 +2,20 @@ from django.db import models
 
 
 class Playbook(models.Model):
-    name = models.CharField(max_length=250)
-    created = models.CharField(max_length=250)
+    id = models.IntegerField(primary_key=True)
+    filename = models.CharField(max_length=250)
 
     def __str__(self):
-        return self.name
+        return self.id
+
+
+class AnsibleTask(models.Model):
+    task_id = models.IntegerField(primary_key=True)
+    playbook_id = models.IntegerField()
+    playbook_name = models.CharField(max_length=250)
+    created = models.DateTimeField(blank=True, auto_now_add=True)
+    finished = models.DateTimeField(blank=True, null=True, default=None)
+    status = models.CharField(max_length=30, default="queued")
+
+    def __str__(self):
+        return str(self.task_id)
--- website/service-cfg-mgnt/cfg_mgnt/ansible_app/views.py
+++ website/service-cfg-mgnt/cfg_mgnt/ansible_app/views.py
@@ -1,7 +1,37 @@
 from django.views.generic import ListView
-from .models import Playbook
-
+from .models import Playbook, AnsibleTask
+from django.forms import forms
+from rest_framework import generics, status
+from .serializers import TaskSerializer
+from.ansible_processor import dupa
+from rest_framework.response import Response
+from rest_framework.renderers import JSONRenderer
+import time
 
 class PlaybookView(ListView):
     model = Playbook
     template_name = 'playbooks/playbook_list.html'
+
+
+class PlaybookDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = AnsibleTask.objects.all()
+    serializer_class = TaskSerializer
+
+
+class TaskView(generics.ListCreateAPIView):
+    queryset = AnsibleTask.objects.all()
+    serializer_class = TaskSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        print(serializer.initial_data["task_id"])
+        time.sleep(2)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+
+        dupa()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
