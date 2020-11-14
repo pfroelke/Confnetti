66afcd177777b554f08c0ca3098dbe05c1c302f0
szymonkomorowski <kontakt.komorowski@gmail.com>
2020-11-14 18:10:40 +0100

    post returns ansible-runner output

--- website/service-cfg-mgnt/cfg_mgnt/ansible_app/models.py
+++ website/service-cfg-mgnt/cfg_mgnt/ansible_app/models.py
@@ -17,6 +17,7 @@ class AnsibleTask(models.Model):
     created = models.DateTimeField(blank=True, auto_now_add=True)
     finished = models.DateTimeField(blank=True, null=True, default=None)
     status = models.CharField(max_length=30, default="queued", unique=False)
+    playbook_file = models.FileField(null=True, blank=True, upload_to="")
 
     def __str__(self):
         return str(self.task_id)
