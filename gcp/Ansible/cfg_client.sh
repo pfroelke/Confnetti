c3f53727f7d8488c358bf0230c20d154abe2d403
pfroelke <patryk.froelke@gmail.com>
2021-02-07 23:14:13 +0100

    task: final cleanup

--- gcp/Ansible/cfg_client.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo sed -i -e '/PubkeyAuthentication/s/yes/no/' /etc/ssh/sshd_config
-sudo sed -i -e '/PasswordAuthentication/s/no/yes/' /etc/ssh/sshd_config
-sudo systemctl restart ssh.service
-# or
-# sudo service ssh restart
