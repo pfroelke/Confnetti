4c593b22707fc06ce6b1cc85637d8f520375c9ea
Pridz <koparka.p@gmail.com>
2021-03-03 15:41:59 +0100

    fix:sshpass instalation

---
+++ service-cfg-mgnt/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# install sshpass
+echo "Install sshpass"
+#apt-get install sshpass -y
+wget https://altushost-swe.dl.sourceforge.net/project/sshpass/sshpass/1.08/sshpass-1.08.tar.gz -O sshpass.tar.gz
+tar -xvf sshpass.tar.gz
+cd ./sshpass-1.08
+./configure
+make install
+cd ../
+
+# make database migrations
+echo "Make database migrations"
+python3 manage.py makemigrations
+
+# Apply database migrations
+echo "Apply database migrations"
+python3 manage.py migrate --noinput
+
+# Start server
+echo "Start server"
+python3 manage.py runserver 0.0.0.0:8000
